7345b14d5e5f157379a93e78cca5689925b7bf22
richardshe01 <richardshe01@gmail.com>
2021-07-26 15:53:07 -0400

    creating landing page

---
+++ src/LandingPage.js
@@ -0,0 +1,10 @@
+function LandingPage(){
+    return(
+        <section>
+            <h1>The Development List</h1>
+            <p>Created by Maurice, Michael, and Richard.</p>
+        </section>
+    );
+}
+
+export default LandingPage;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import LandingPage from './LandingPage';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/*<App />*/}
+    <LandingPage/>
   </React.StrictMode>,
   document.getElementById('root')
 );
